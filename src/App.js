ed50c90329f58a8efc07aefc303fce6b4f8e26a3
TX-9 <jacobleecs@gmail.com>
2021-11-11 15:07:18 -0500

    split screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { SplitScreen } from "./SplitScreen";
+
+const LeftHandComponent = ({ name }) => {
+	return <h1 style={{backgroundColor: 'green'}}>{name}</h1>;
+};
+const RightHandComponent = ({ message }) => {
+	return <h1 style={{backgroundColor: 'blue'}}>{message}</h1>;
+}
+
 function App() {
-  return (
-    <p>code here</p>
-  );
+	return (
+		<SplitScreen 
+			leftWeight={1}
+			rightWeight={3} >
+				<LeftHandComponent name="Jacob"/>
+				<RightHandComponent message="Hello"/>
+		</SplitScreen>
+	);
 }
 
 export default App;
