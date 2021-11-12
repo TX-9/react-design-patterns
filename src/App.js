7afa6d91a81068a9c4f84687793b0c8f133158e9
TX-9 <jacobleecs@gmail.com>
2021-11-12 15:30:27 -0500

    HOC for http operations

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { printProps } from "./printProps";
 import { UserInfo } from "./UserInfo";
+import { UserInfoForm } from "./UserInfoForm";
+import { withUser } from "./withUser";
 
-const UserInfoWrapped = printProps(UserInfo);
+const UserInfoWithLoader = withUser(UserInfo, '123');
 
 function App() {
   return (
-    <UserInfoWrapped a={1} b="Hello" c={{name:"Jacob"}} />
+    <UserInfoForm />
   );
 }
 
