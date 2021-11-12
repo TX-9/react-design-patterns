f07d1919b2cf836a31f1a0e6ea17d8217d99fce1
TX-9 <jacobleecs@gmail.com>
2021-11-12 15:52:55 -0500

    add useCurrentUser

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import { printProps } from "./printProps";
 import { UserInfo } from "./UserInfo";
-import { UserInfoForm } from "./UserInfoForm";
-import { withUser } from "./withUser";
 
-const UserInfoWithLoader = withUser(UserInfo, '123');
 
 function App() {
   return (
-    <UserInfoForm />
+    <UserInfo />
   );
 }
 
