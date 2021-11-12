1f2769a74260417a26ec94abc90d0d61f861dd2d
TX-9 <jacobleecs@gmail.com>
2021-11-12 10:22:04 -0500

    add Data source

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import { CurrentUserLoader } from "./CurrentUserLoader";
 import { UserInfo } from "./UserInfo";
 import { ProductInfo } from "./ProductInfo";
 import { ResourceLoader} from "./ResourceLoader";
+import { DataSource } from "./DataSource";
+import axios from "axios";
+
+const getServerData = url => async () => {
+  const response = await axios.get(url);
+  return response.data;
+};
 
 function App() {
   return (
     <>
-    <ResourceLoader resourceUrl="/users/123" resourceName="user">
+    <DataSource getDataFunc={getServerData("/users/123")} resourceName="user">
       <UserInfo />
-    </ResourceLoader>
-    <ResourceLoader resourceUrl="/products/1234" resourceName="product">
-      <ProductInfo />
-    </ResourceLoader>
+    </DataSource>
     </>
   );
 }
