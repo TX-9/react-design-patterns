040a39560feee6a265d55a90f376c0d8e415be34
TX-9 <jacobleecs@gmail.com>
2021-11-12 13:18:30 -0500

    add uncontrolled onboarding flow

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
+import { UncontrolledOnboardingFlow } from "./UncontrolledOnboardingFlow";
+
+const StepOne = ({ goToNext }) => (
+  <>
+  <h1>Step 1</h1>
+  <button onClick={() => goToNext({name: "Jacob Lee"})}>Next</button>
+  </>
+);
+const StepTwo = ({ goToNext }) => (
+  <>
+  <h1>Step 2</h1>
+  <button onClick={() => goToNext({age: 30})}>Next</button>
+  </>
+  );
+const StepThree = ({ goToNext }) => (
+  <>
+  <h1>Step 3</h1>
+  <button onClick={() => goToNext({hairColor: "black"})}>Next</button>
+  </>
+  );
 function App() {
   return (
-    <p>code here</p>
+    <UncontrolledOnboardingFlow onFinish={data => {
+      console.log(data);
+      alert("Onboarding Completed");
+      }}>
+      <StepOne />
+      <StepTwo />
+      <StepThree />
+    </UncontrolledOnboardingFlow>
   );
 }
 
