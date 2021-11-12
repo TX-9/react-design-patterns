522fa02f4ad0960e93fa46ed06fec79e91876893
TX-9 <jacobleecs@gmail.com>
2021-11-12 16:11:06 -0500

    add useDataSource which is most generalized

--- src/UserInfo.js
+++ src/UserInfo.js
@@ -1,12 +1,28 @@
+import axios from "axios";
 import { useCurrentUser } from "./useCurrentUser";
+import { useDataSource } from "./useDataSource";
 import { useResource } from "./useResource";
 import { useUser } from "./useUser";
 
+const serverResource = resourceUrl => async () => {
+	const response = await axios.get(resourceUrl);
+	return response.data;
+};
+
+const localStorageResource = key => () => {
+	return localStorage.getItem(key);
+};
 export const UserInfo = ({ userId }) => {
 	// const user = useCurrentUser();
 	// const user = useUser(userId);
-	const user = useResource(`/users/${userId}`);
-
+	// const user = useResource(`/users/${userId}`);
+	// const user = useDataSource(async () => {
+	// 	const response = await axios.get(`/users/${userId}`);
+	// 	return response.data;
+	// });
+	const user = useDataSource(serverResource(`/users/${userId}`));
+	const message = useDataSource(localStorageResource('message'));
+	
 	const { name, age, hairColor, hobbies } = user || {};
 
 	return user ? (
