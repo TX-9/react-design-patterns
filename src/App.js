09ce7672446396b894a76f0d93a656300f0cbb00
TX-9 <jacobleecs@gmail.com>
2021-11-12 15:57:26 -0500

    add useUser

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import { UserInfo } from "./UserInfo";
 
 function App() {
   return (
-    <UserInfo />
+    <>
+    <UserInfo userId="123"/>
+    <UserInfo userId="234"/>
+    <UserInfo userId="345"/>
+    </>
   );
 }
 
