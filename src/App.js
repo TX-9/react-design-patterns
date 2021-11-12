3cf7e75dd92736e047fc6acfd81a178d2a5c3806
TX-9 <jacobleecs@gmail.com>
2021-11-12 13:38:37 -0500

    add Controlled onboarding flow

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { UncontrolledOnboardingFlow } from "./UncontrolledOnboardingFlow";
+import { useState } from "react";
+import { ControlledOnboardingFlow } from "./ControlledOnboardingFlow";
 
 const StepOne = ({ goToNext }) => (
   <>
@@ -15,19 +16,33 @@ const StepTwo = ({ goToNext }) => (
 const StepThree = ({ goToNext }) => (
   <>
   <h1>Step 3</h1>
+  <p>Congratulations! You qualify for your senior discount</p>
+  <button onClick={() => goToNext({})}>Next</button>
+  </>
+  );
+const StepFour = ({ goToNext }) => (
+  <>
+  <h1>Step 4</h1>
   <button onClick={() => goToNext({hairColor: "black"})}>Next</button>
   </>
   );
 function App() {
+  const [onboardingData, setOnboardingData] = useState({});
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const onNext = (stepData) => {
+    setCurrentIndex(prev => prev+1);
+    setOnboardingData({...onboardingData, ...stepData});
+
+  };
   return (
-    <UncontrolledOnboardingFlow onFinish={data => {
-      console.log(data);
-      alert("Onboarding Completed");
-      }}>
+    <ControlledOnboardingFlow 
+      currentIndex={currentIndex}
+      onNext={onNext}>
       <StepOne />
       <StepTwo />
-      <StepThree />
-    </UncontrolledOnboardingFlow>
+      {onboardingData.age >= 62 && <StepThree />}
+      <StepFour />
+    </ControlledOnboardingFlow>
   );
 }
 
